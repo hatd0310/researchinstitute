0255d365f2ca5b44283c0c9e234610ce7fbbf714
hatd0310 <tranduyha2000@gmail.com>
2024-09-12 22:44:17 +0700

    update

--- researchinstitute/problems/TwoSum.cpp
+++ researchinstitute/problems/TwoSum.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 /*
-Given an array of integers nums and an integer target,
-return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution,
-and you may not use the same element twice.
-
-You can return the answer in any order.
-
-https://leetcode.com/problems/two-sum/
-*/
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice.
+ * You can return the answer in any order.
+ * Source: https://leetcode.com/problems/two-sum/
+ *
+ * Follow-up: Can you come up with an algorithm that is less than O(n**2) time
+ * complexity ?
+ */
 
 class TwoSum {
 
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum_rev1(vector<int>& nums, int target) {
 
     }
 };
