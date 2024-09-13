e6b04de093be118b5e2518e05181301365e34109
hatd0310 <tranduyha2000@gmail.com>
2024-09-13 17:29:04 +0700

    update

--- researchinstitute/cinstitute/thefirstprogram.c
+++ researchinstitute/cinstitute/thefirstprogram.c
@@ -1,12 +1,32 @@
 // #(hash) preprocessor directive
 #include <stdio.h>
 
-int main() {
-    puts("It's me, your first program");
+// Symbolic Constants
+#define PI 3.14159
+#define MAX_BUFFER_SIZE 1024
 
+#define LOWER 0     /* lower limit of table */
+#define UPPER 300   /* upper limit */
+#define STEP  0     /* step size */
+
+
+
+int main() {
+    /*
+    float floatingPoint = 3.14f;
+    printf("floatingPoint = %.1f", floatingPoint);
+    printf("UPPER = %f", PI);
+    */
+    
+    //int c = getchar(); // 
+    //while (c != EOF) {
+    //    putchar(c); 
+    //    c = getchar();
+    //    
+    //}
+    
 
     return 0;
-
 }
 
 
