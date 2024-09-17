1b0c822d7270c56ac85d767743e163f7341b1675
hatd0310 <tranduyha2000@gmail.com>
2024-09-17 17:12:09 +0700

    udpate

--- researchinstitute/practices_cpp/hackerrank.cpp
+++ researchinstitute/practices_cpp/hackerrank.cpp
@@ -4,29 +4,7 @@ using namespace std;
 
 
 int main() {
-	/*
-	 * ===== Fundamental Data Types: =====
-	 * 
-	 * Data Type=====Format Specifier===Typical Size(bytes)
-	 * char				%c					1
- 	 * unsigned short	%hu					2
- 	 * short			%hd					2
-	 * unsigned int		%u					8 (64 - bit) or 4 (32 - bit)
-	 * unsigned long	%lu					8 (64 - bit) or 4 (32 - bit)
-	 * int				%d					8 (64 - bit) or 4 (32 - bit)
-	 * long				%ld					8 (64 - bit) or 4 (32 - bit)
-	 * float			%f					4
-	 * double			%lf					8
-	 * 
-	 */
-	 /*
-	 * ===== Format Specifiers: =====
-	 * %x	Hexadecimal
-	 * %o	Octal
-	 * %e	Scientific notation for floating (1.234e+05)
-	 * %g	Shorter of %f or %e
-	 * %p	Pointer address (hexadecimal format)
-	 */
+	
 	
 	
 	return 0;
--- researchinstitute/practices_cpp/pointers.cpp
+++ researchinstitute/practices_cpp/pointers.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 
-
-void increment(int *v) {
-    (*v)++;
-}
-
 int main() {
-    int a;
-    cin >> a;
-    increment(&a);
-    printf("%d", a);
+    
+    int num = 42;
+    int* ptr = &num;  // ptr holds the address of num
+
+    cout << "Value of num: " << num << "\n";
+    cout << "Address of num: " << &num << "\n";
+    cout << "Pointer ptr points to address: " << ptr << "\n";
+    cout << "Value at address ptr points to: " << *ptr << "\n";
     return 0;
 }
