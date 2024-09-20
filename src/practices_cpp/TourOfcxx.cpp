f43fb0bd04256d0b83bad3cfb6bac5dd67eb72e7
hatd0310 <tranduyha2000@gmail.com>
2024-09-21 01:29:38 +0700

    update

--- src/practices_cpp/TourOfcxx.cpp
+++ src/practices_cpp/TourOfcxx.cpp
@@ -8,11 +8,11 @@ double sum(std::vector<double> numbers) {
         total += numbers.at(i);
         std::cout << "vector.at(" << i << ")" << " = Address: " << &numbers.at(i) << "\n";
         std::cout << "\n";
-       
+
     }
     numbers.push_back(total);
 
-    
+
 
     return total;
 }
@@ -24,18 +24,17 @@ int main() {
     int var = 17;
     constexpr int dmv = 17;       // evaluated at compile time
     const double sqv = sqrt(var); // possibly computed at run time
-    
+
     //std::cout << "Value of sqv is: " << sqv << "\n";
     //std::cout << "Value of dmv is: " << dmv << "\n";
 
     std::vector<double> v = { 1.2, 3.4, 4.5 };  // v is not a constant
     //const double s1 = sum(v);           // OK: sum(v) is evaluated at run time
     //constexpr double s2 = sum(v);       // error: sum(v) is not a constant expression
-   
+
     //std::cout << "Value of var s1 is: " << s1 << "\n";
     //std::cout << "Value of var s2 is: " << s2 << "\n";
     double a = 1.1;
-    v.insert(0, a);
     // Iterate over each element and print it
     for (int number : v) {
         std::cout << number << " ";
@@ -43,7 +42,7 @@ int main() {
 
     std::cout << "\n";
     //sum(v);
-    
+
 
     /* Chapter 1.7 Pointers, Arrays, and References */
 
---
+++ src/practices_cpp/database_connection.cpp
@@ -0,0 +1,16 @@
+#include "../researchinstitute.h"
+#include <stdlib.h>
+#include <libpq-fe.h>
+
+/* https://dev.to/omarsaad/building-a-postgresql-database-client-with-libpq-in-c-connecting-and-executing-queries-5a51 */
+
+int main(int argc, char *argv[]) {
+    std::cout << "libpq tutorial" << "\n";
+
+
+
+
+
+
+}
+
