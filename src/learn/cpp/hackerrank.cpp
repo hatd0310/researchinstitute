a714ed660f22fe93fbc9e9c14b9de0cc6fffdb05
hatd0310 <tranduyha2000@gmail.com>
2024-09-26 17:19:09 +0700

    update

--- src/learn/cpp/hackerrank.cpp
+++ src/learn/cpp/hackerrank.cpp
@@ -7,10 +7,13 @@
 
 
 int main() {
-    int n; // n-elements array a;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n, index; // n-elements array a
     int a[n];
-
-
-
+    
+    for (index = 0; index < n; ++index) {
+        // each index contain a refer to an array
+        
+    }
     return 0;
 }
