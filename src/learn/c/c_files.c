742e169fd01bef113192e79b04959b2fbb11217b
hatd0310 <tranduyha2000@gmail.com>
2024-10-11 18:06:11 +0700

    update

---
+++ src/learn/c/c_files.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main() {
+	
+}
+
+
+void read_file() {
+
+}
+
+void write_file() {
+	FILE* fptr;
+
+	// Open a file in writing mode
+	fptr = fopen("filename.txt", "w"); // fopen(filename, mode)
+
+	// Write some text to the file
+	fprintf(fptr, "Some text");
+
+	// Close the file
+	fclose(fptr);
+}
