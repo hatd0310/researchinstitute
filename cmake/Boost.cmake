0cbaba900d0241633bb3f84fb8a271c62c0dd00f
hatd0310 <tranduyha2000@gmail.com>
2025-01-26 12:42:53 +0700

    update

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -5,17 +5,10 @@ set(Boost_USE_MULTITHREADED      ON)
 set(Boost_USE_STATIC_RUNTIME    OFF)
 set(Boost_NO_BOOST_CMAKE 		 ON)  # to disable the search for boost-cmake
 set(Boost_NO_SYSTEM_PATHS 		 ON)
+
 set(BOOST_ROOT $ENV{BOOST_ROOT})
-set(Boost_DIR ${BOOST_ROOT})
-set(BOOST_LIBRARYDIR ${BOOST_ROOT}/libs)
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${BOOST_ROOT}) 
-set(Boost_ADDITIONAL_VERSIONS 1.87.0)
+set(Boost_LIBRARY_DIRS ${BOOST_ROOT}/libs)
+set(Boost_INCLUDE_DIRS ${BOOST_ROOT})
 
+message(STATUS "Found BOOST_ROOT: ${BOOST_ROOT}")
 
-find_package(Boost REQUIRED)
-
-
-if(Boost_FOUND)
-  message(STATUS "Found BOOST_ROOT: ${BOOST_ROOT}")
-  message(STATUS "Found Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
-endif()
