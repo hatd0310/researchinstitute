351dfe9894dde48e589e3990c2eb023fd7ea8958
hatd0310 <tranduyha2000@gmail.com>
2025-02-03 16:05:43 +0700

    update

---
+++ src/solving_problem/leetcode/linked_list.cpp
@@ -0,0 +1,16 @@
+#include "node.cpp"
+/*
+    ----------        ----------
+    | Data   |        | Data   |
+    |--------|        |--------|
+    | Pointer| - - -> | Pointer|
+    ----------        ----------
+
+*/
+
+template <typename T>
+class LinkedList
+{
+public:
+    T add
+};
--- src/solving_problem/leetcode/node.cpp
+++ src/solving_problem/leetcode/node.cpp
@@ -1,28 +1,48 @@
-// Definition for singly-linked list.
-struct ListNode {
+/*
+    Define user-defind data types ( also known as aggregate types )
+    - struct: Members are public by default.
+    - class:  Members are private by default.
+
+    private can only be accessed by the member function of the class or by friends
+*/
+
+// Definition for singly-linked list of leetcode solving problem.
+struct ListNode
+{
     int val;
-    ListNode* next;
+    ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-// Definition for singly-linked list.
-struct Node_SLL {
-    public:
-        int val;
-        Node_SLL* next;
-        Node_SLL() : val(0), next(nullptr) {}
-        Node_SLL(int val) : val(val), next(nullptr) {}
-        Node_SLL(int x, Node_SLL* next) : val(x), next(next) {}
+// Definition for singly-linked list of Udemy E-learning.
+struct Node_SLL
+{
+    int val;
+    Node_SLL *next;
+    Node_SLL() : val(0), next(nullptr) {}
+    Node_SLL(int val) : val(val), next(nullptr) {}
+    Node_SLL(int x, Node_SLL *next) : val(x), next(next) {}
 };
 
-// Definition for doubly-linked list.
-struct Node_DLL {
-    public:
-        int val; // to store the Value or data
-        Node_DLL* next; // Pointer to point the Next element
-        Node_DLL* prev; // Pointer to point the Previous element
-        Node_DLL(int val) : val(val), next(nullptr), prev(nullptr) {}
-        Node_DLL(int x, Node_DLL* next, Node_DLL* prev) : val(x), next(next), prev(prev) {}
+// Definition for doubly-linked list of Udemy E-learning.
+struct Node_DLL
+{
+    int val;        // to store the Value or data
+    Node_DLL *next; // Pointer to point the Next element
+    Node_DLL *prev; // Pointer to point the Previous element
+    Node_DLL(int val) : val(val), next(nullptr), prev(nullptr) {}
+    Node_DLL(int x, Node_DLL *next, Node_DLL *prev) : val(x), next(next), prev(prev) {}
+};
+
+// Definition for template datatype node
+template <typename T>
+class Node
+{
+public:
+    T data;
+    Node<T> *next_node;
+    Node(T data) : data(data), next_node(nullptr) {}
+    Node(T data, Node<T> node) : data(data), next_node(node) {}
 };
