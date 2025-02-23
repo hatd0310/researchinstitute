13813d973d000e45fcc8fa01a0f40a9d6a54163f
hatd0310 <tranduyha2000@gmail.com>
2025-02-23 18:06:26 +0700

    updage

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -1,14 +1,14 @@
-set(Boost_USE_STATIC_LIBS        ON)  # only find static libs
-set(Boost_USE_DEBUG_LIBS        OFF)  # ignore debug libs and
-set(Boost_USE_RELEASE_LIBS       ON)  # only find release libs
-set(Boost_USE_MULTITHREADED      ON)
+set(Boost_USE_STATIC_LIBS       ON)  # only find static libs
+set(Boost_USE_DEBUG_LIBS        OFF) # ignore debug libs and
+set(Boost_USE_RELEASE_LIBS      ON)	 # only find release libs
+set(Boost_USE_MULTITHREADED		ON)
 set(Boost_USE_STATIC_RUNTIME    OFF)
-set(Boost_NO_BOOST_CMAKE 	 ON)  # to disable the search for boost-cmake
-set(Boost_NO_SYSTEM_PATHS 	 ON)
+set(Boost_NO_BOOST_CMAKE 		ON)  # to disable the search for boost-cmake
+set(Boost_NO_SYSTEM_PATHS 		ON)
 
-set(BOOST_ROOT $ENV{BOOST_ROOT})
-set(Boost_LIBRARY_DIRS ${BOOST_ROOT}/libs)
-set(Boost_INCLUDE_DIRS ${BOOST_ROOT})
+set(Boost_ROOT $ENV{BOOST_ROOT})
+set(Boost_LIBRARY_DIRS ${Boost_ROOT}/libs)
+set(Boost_INCLUDE_DIRS ${Boost_ROOT})
 
-message(STATUS "Found BOOST_ROOT: ${BOOST_ROOT}")
+message(STATUS "Found Boost_ROOT: ${Boost_ROOT}")
 
--- cmake/PostgreSQL.cmake
+++ cmake/PostgreSQL.cmake
@@ -1,1 +1,4 @@
+set(PostgreSQL_ADDITIONAL_VERSIONS "17.4")
+set(PostgreSQL_ROOT $ENV{POSTGRESQL_HOME})
+
 find_package(PostgreSQL REQUIRED)
